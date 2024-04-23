609b75bcc845bf61bcc1d48c793ac89159a06d37
fallsvc <fallsvc9@gmail.com>
2024-04-23 22:27:34 +0800

    Java

---
+++ test/src/test.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class test {
+    public static void main(String[] args) 
+	{
+//        System.out.println("hello world#n");
+        int i=0;
+        for(i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+        int a=1_000;
+        System.out.println(a);
+
+    }
+
+}
