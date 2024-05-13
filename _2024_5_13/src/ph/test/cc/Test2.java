c6a58b39b73b526c140bc2c51974dcdb2795310a
fallsvc <fallsvc9@gmail.com>
2024-05-13 23:26:30 +0800

    java

---
+++ _2024_5_13/src/ph/test/cc/Test2.java
@@ -0,0 +1,15 @@
+package ph.test.cc;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/5/13  21:04
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+        dog.name="ag";
+        dog.bark();
+        dog.eat();
+    }
+}
