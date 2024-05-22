10c15fe7abd6338c8ea66f13b0352aafda92b2d9
fallsvc <fallsvc9@gmail.com>
2024-05-22 23:50:07 +0800

    java

---
+++ _2024_5_22/src/Demo3/Animal.java
@@ -0,0 +1,13 @@
+package Demo3;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/5/22  23:12
+ */
+public abstract class Animal {
+    public String name;
+    public int age;
+    public abstract void eat();
+
+}
