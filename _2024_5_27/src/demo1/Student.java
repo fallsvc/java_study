d6d71f54795c1915a9004b0d62ec62d1d539dc3a
fallsvc <fallsvc9@gmail.com>
2024-05-27 23:51:46 +0800

    java

---
+++ _2024_5_27/src/demo1/Student.java
@@ -0,0 +1,60 @@
+package demo1;
+
+import java.util.Objects;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/5/27  18:44
+ */
+public class Student implements Comparable<Student>,Cloneable{
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
