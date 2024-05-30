c5089af4ff08a3198d2f33e5169827309eaf6ec0
fallsvc <fallsvc9@gmail.com>
2024-05-30 23:55:32 +0800

    java

---
+++ _2024_5_30/src/demo1/NameException.java
@@ -0,0 +1,13 @@
+package demo1;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/5/30  22:12
+ */
+public class NameException extends Exception{
+
+    public NameException(){
+        super();
+    }
+}
