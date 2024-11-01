a71c86e3afef03934814804678d9d52b371314e7
fallsvc <fallsvc9@gmail.com>
2024-11-01 21:39:46 +0800

    java

---
+++ JavaEE/src/main/java/file/bytestream/Demo1.java
@@ -0,0 +1,51 @@
+package file.bytestream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/11/1  20:21
+ */
+public class Demo1 {
+    public static void main(String[] args) throws IOException {
+//        InputStream inputStream=null;
+//       try{
+//           inputStream=new FileInputStream("./test.txt");
+//       }finally{
+//           // finally关闭资源
+//           inputStream.close();
+//       }
+        // try with resource 实现closable接口才能放入()
+        try(InputStream inputStream=new FileInputStream("./test.txt")){
+            // 读文件
+            // 一次读一个字节
+//            while(true){
+//                int data=inputStream.read();
+//                if(data==-1){
+//                    // 读完
+//                    break;
+//                }
+//                System.out.println(data);
+//            }
+            while (true) {
+                // 一次读多个字节
+                byte[] data=new byte[3];
+                // n 实际读取字节数
+                int n=inputStream.read(data);
+                System.out.println("n="+n);
+                if(n==-1){
+                    // 读完
+                    break;
+                }
+                for (int i=0;i<n;i++) {
+                    System.out.printf("%x\n",data[i]);
+                }
+                System.out.println("============================");
+            }
+        }
+    }
+}
--- JavaEE/src/main/java/file/first/Demo1.java
+++ JavaEE/src/main/java/file/first/Demo1.java
@@ -2,6 +2,7 @@ package file.first;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @auther falls_vc
@@ -32,11 +33,47 @@ public class Demo1 {
 
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main2(String[] args) throws IOException {
         File file=new File("./test.txt");
         file.createNewFile();
         System.out.println(file.exists());
         System.out.println(file.isFile());
         System.out.println(file.isDirectory());
+    }
+
+    public static void main3(String[] args) throws IOException, InterruptedException {
+        File file=new File("./test.txt");
+//        boolean ret=file.delete();// 删除文件
+        file.createNewFile();
+        // 进程结束删除文件
+//        file.deleteOnExit();
+//        System.out.println();
+//        Thread.sleep(10000);
+    }
+
+    public static void main4(String[] args) {
+        File file =new File("C:/");
+
+        String[] ret=file.list();
+        System.out.println(Arrays.toString(ret));
+
+        File[] ret1=file.listFiles();
+        System.out.println(Arrays.toString(ret1));
+    }
+
+    public static void main5(String[] args) {
+        File file=new File("./test");
+
+//        boolean result=file.mkdir();//// 单个目录
+//        System.out.println(result);
+        file.delete();
+//        file.mkdirs();// 多级目录
+    }
+
+    public static void main(String[] args) {
+        File file=new File("./test.txt");
+        File file1=new File("./src/test.txt");
+//        file.renameTo(file1);
+        file1.renameTo(file);
     }
 }
