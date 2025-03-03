bf73935ed4a8f1cc7aa089fe476f9dfdfe8e2622
fallsvc <fallsvc9@gmail.com>
2025-03-03 10:53:28 +0800

    boot

--- springboot/src/main/java/com/falls/demos/RequestController.java
+++ springboot/src/main/java/com/falls/demos/RequestController.java
@@ -1,5 +1,9 @@
 package com.falls.demos;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,8 +37,11 @@ public class RequestController {
     }
 
     /**
-     * requestParam 默认必传参数不会null
-     * @param keyword
+     * @RequestParam 默认必传参数不会null
+     * 可以重命名参数
+     * require参数默认为true 必传参数 只能以重命名传参
+     * 如果require设为false则就不为必传参数
+     * @param keyword - q
      * @return
      */
     @RequestMapping("/r4")
@@ -48,6 +55,7 @@ public class RequestController {
     }
 
     @RequestMapping("/r6")
+    //这里参数绑定list为必传参数
     public String r6(@RequestParam List<Integer> list){
         return list.toString();
     }
@@ -59,10 +67,16 @@ public class RequestController {
     }
 
     @RequestMapping("/r8/{type}/{article}")
-    public String r8(@PathVariable Integer article,@PathVariable String type){
-        return "articleID"+article+",type:"+type;
+    public String r8(@PathVariable Integer article,@PathVariable("type") String thetype){
+        return "articleID"+article+",type:"+thetype;
     }
 
+    /**
+     * RequestPart 重命名
+     * @param file1
+     * @return
+     * @throws IOException
+     */
     @RequestMapping("/r9")
     public String r9(@RequestPart("file")MultipartFile file1) throws IOException {
         System.out.println(file1.getOriginalFilename());
@@ -70,6 +84,67 @@ public class RequestController {
         file1.transferTo(new File("C:\\Users\\12092\\Desktop\\software\\"+file1.getOriginalFilename()));
 
         return "文件上传成功";
+    }
+    @RequestMapping("/r10")
+    public String r10(HttpServletRequest request, HttpServletResponse response){
+        Cookie[] cookies= request.getCookies();
+        if(cookies!=null){
+            for (Cookie cookie: cookies) {
+                System.out.println(cookie.getName()+":"+cookie.getValue());
+            }
+        }
+        return "返回Cookie成功";
+    }
+    @RequestMapping("/r11")
+    public String r11(@CookieValue("java")String java){
+        System.out.println(java);
+        return "Cookie中java的值="+java;
+    }
+    @RequestMapping("/setSession")
+    public String setSession(HttpServletRequest request){
+        // 从cookie中获取sessionId,根据sessionId获得session对象
+        HttpSession session=request.getSession();
+        //默认存储在储存中
+        session.setAttribute("name","zhangsan");
+        session.setAttribute("age",12);
+
+        return "设置成功";
+    }
+    @RequestMapping("/getSession")
+    public String getSession(HttpServletRequest request){
+        // 从cookie中获取sessionId,根据sessionId获得session对象
+        HttpSession session=request.getSession();
+        // 如果用户登录有值，否则session为null
+        if(session==null){
+            return "用户未登录";
+        }else{
+            String userName=(String)session.getAttribute("name");
+            return "用户名"+userName;
+        }
+    }
+    @RequestMapping("/getSession2")
+    public String getSession2(HttpSession session){
+        // 如果用户登录有值，否则session为null
+        if(session==null){
+            return "用户未登录";
+        }else{
+            String userName=(String)session.getAttribute("name");
+            return "用户名"+userName;
+        }
+    }
+     @RequestMapping("/getSession3")
+    public String getSession3(@SessionAttribute("name") String userName){
+            return "用户名"+userName;
+    }
+
+    @RequestMapping("/getHeader")
+    public String getHeader(HttpServletRequest request){
+        String userAgent=request.getHeader("User-Agent");
+        return "header中的useragent"+userAgent;
+    }
+    @RequestMapping("/getHeader2")
+    public String getHeader2(@RequestHeader("User-Agent") String userAgent){
+        return "header中的useragent"+userAgent;
     }
 }
 
---
+++ springboot/src/test/java/com/falls/demos/JsonTest.java
@@ -0,0 +1,41 @@
+package com.falls.demos;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @auther falls_vc
+ * description:
+ * @date 2024/11/30  11:20
+ */
+public class JsonTest {
+    @Test
+    public void testJSON(){
+        System.out.println(1233);
+    }
+    @Test
+    public void testJSON2(){
+        System.out.println(4556);
+    }
+    @Test
+    void testJsonObject1() throws JsonProcessingException {
+        UserInfo userInfo=new UserInfo("zhangsan","lisi",20);
+        ObjectMapper objectMapper=new ObjectMapper();
+        // 转Json
+        String s=objectMapper.writeValueAsString(userInfo);
+
+        System.out.println(s);
+    }
+
+    @Test
+    void testJsonObject2() throws JsonProcessingException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        // Json转对象
+        String s="{\"name\":\"zhangsan\",\"age\":20,\"id\":\"lisi\"}";
+        // readValue先new UserInfo() 对象 再进行赋值 此时如果UserInfo没有无参构造时会报错
+        UserInfo userInfo=objectMapper.readValue(s,UserInfo.class);
+
+        System.out.println(userInfo);
+    }
+}
