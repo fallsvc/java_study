057ecd13b5bc6caffb7e9bf3a52f412ebb41791d
fallsvc <fallsvc9@gmail.com>
2025-05-21 20:16:29 +0800

    java

--- GreetAlgorithmProblem/src/main/java/optimizationalgorithm/bfs/Demo1.java
+++ GreetAlgorithmProblem/src/main/java/optimizationalgorithm/bfs/Demo1.java
@@ -1,7 +1,9 @@
 package optimizationalgorithm.bfs;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * @auther falls_vc
@@ -48,7 +50,7 @@ public class Demo1 {
         int[] dy={-1,1,0,0};
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         char[][] a={{'1','2','3'},{'2','2'}};
         System.out.println(a.length);
 
@@ -209,6 +211,118 @@ public class Demo1 {
                     }
                 }
             }
+        }
+    }
+
+    class Solution5 {
+//        https://leetcode.cn/problems/nearest-exit-from-entrance-in-maze/
+//        最近迷宫出口
+
+        int m;
+        int n;
+        boolean[][] visit;
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
+
+        public int nearestExit(char[][] maze, int[] entrance) {
+            m=maze.length;
+            n=maze[0].length;
+            visit=new boolean[m][n];// 是否经过
+
+            return  bfs(maze,entrance);
+        }
+        public int bfs(char[][] maze,int[] entrance) {
+            int count=-1;
+
+            Queue<int[]> q=new ArrayDeque<>();
+            visit[entrance[0]][entrance[1]]=true;
+            q.offer(new int[]{entrance[0],entrance[1]});
+
+            while(!q.isEmpty()) {
+                count++;
+                int size=q.size();
+                for(int i=0;i<size;i++) {
+                    int[] t=q.poll();
+                    for(int k=0;k<4;k++) {
+                        int x=dx[k]+t[0],y=dy[k]+t[1];
+                        if(x>=0 && x<m &&y>=0 && y<n) {
+                            // 是否能再走
+                            if(maze[x][y]=='.' &&visit[x][y]==false)
+                                q.offer(new int[]{x,y});
+                            visit[x][y]=true;
+                        }else{ // 下一个越界
+                            //能出
+                            if(count!=0) return count;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main2(String[] args) {
+        char[] arr={'a','a'};
+        StringBuilder stringBuilder=new StringBuilder("asd");
+        String s="asd";
+        System.out.println(stringBuilder.toString().equals(s));
+        Set<Integer> set=new HashSet<>();
+        System.out.println(arr.toString());
+
+    }
+
+    public static void main(String[] args) {
+        String[] a={"AACCGATT","AACCGATA","AAACGATA","AAACGGTA"};
+//        minMutation("AACCGGTT","AAACGGTA",a);
+    }
+    class Solution6 {
+//        https://leetcode.cn/problems/minimum-genetic-mutation/
+//        最小基因变化
+        char[] ge={'A','C','G','T'};
+        public int minMutation(String startGene, String endGene, String[] bank){
+            if(bank.length==0) return -1;// 边界
+            Queue<String> q=new ArrayDeque<>();
+            q.offer(startGene);
+            char[] end=endGene.toCharArray();
+            // 存放bank
+            Set<String> set=new HashSet();
+            // 已经遍历过的
+            Set<String> set2=new HashSet();
+            set2.add(startGene);
+
+            for(String x : bank) {
+                set.add(x);
+            }
+            int count=0;// 返回值
+
+            while(!q.isEmpty()) {
+                count++;
+                int size=q.size();
+                for(int j=0;j<size;j++) {
+                    String t=q.poll();
+                    char[] tmp=t.toCharArray();
+                    for(int i=0;i<end.length;i++) {
+                        for(int k=0;k<4;k++) {
+                            if(tmp[i]!=ge[k]) {
+                                char ch=tmp[i];// 等下用于还原数组
+                                tmp[i]=ge[k];
+                                String a=new String(tmp);
+                                if(set.contains(a)&&!set2.contains(a)) {
+                                    // 判断返回
+                                    if(endGene.equals(a)) {
+                                        return count;
+                                    }
+                                    q.offer(a);
+                                    set2.add(a);
+                                }
+                                tmp[i]=ch;// 不能在过程中continue 不然会使本来可以复原的数据 无法复原
+                            }
+                        }
+                    }
+                }
+
+            }
+            return -1;
         }
     }
 }
